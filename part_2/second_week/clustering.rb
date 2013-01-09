16ed356eaca8b344f42d9a0a4d09326ad93107d5
Dmitrii Samoilov <e2718281828@ya.ru>
2013-01-09 20:20:36 +0200

    changes in part_2/second_week/task 1

--- part_2/second_week/clustering.rb
+++ part_2/second_week/clustering.rb
@@ -1,7 +1,9 @@
+require 'set'
+
 edges = {}
 vertices = {}
 
-Edge = Struct.new :node1, :node2, :cost
+Edge = Struct.new :node1, :node2, :cost, :scc
 Vertex = Struct.new :node, :leader
 
 File.open('clustering1.txt').each_with_index do |line, edge_index|
@@ -16,23 +18,32 @@ File.open('clustering1.txt').each_with_index do |line, edge_index|
   vertices[v2] ||= []
   vertices[v2].push edge_index
   
-  edge = Edge.new(v1, v2, cost)
+  edge = Edge.new(v1, v2, cost, nil) # link to scc would be nil for now
   edges[edge_index] = edge
 end
 
+puts 'imported all edges'
+
 class SCC
-  def initialize()
-    @vertices_count = 0
+
+  attr_accessor :leader
+
+  def initialize(mst)
     @edges = []
+    @mst = mst
     @vertices = Set.new []
-    @leader = nil
   end
   
+  def count
+    @vertices.count
+  end
+
   # checking whether the edge has some connections with this particular SCC
   def is_fit?(edge)
     @vertices.any?{|v| v.node == edge.node1.node || v.node == edge.node2.node}
   end
   
+  # in fact it could be merge of two SCCs
   def push_edge(edge)
     if @leader.nil?
       @leader = edge.node1.leader
@@ -41,76 +52,74 @@ class SCC
       edge.node1.leader = @leader
       edge.node2.leader = @leader
     end
-    
-    @vertices_count += 2
-    @vertices << edge.node1
-    @vertices << edge.node2
-    @edges << edge
-  end
-  
-  def merge(other)
-    if @vertices_count > other.vertices_count
-      # change leaders of the other SCC
-      other.vertices.each{ |v| v.node.leader = @leader }
-      @vertices_count += other.vertices_count
+#puts 'edge.scc - ' + edge.scc.inspect
+    if edge.scc
+      puts "@vertices.count - #{@vertices.count}, edge.scc.count - #{edge.scc.count}"
+      if @vertices.count > edge.scc.count
+        # change leaders of the other SCC
+        edge.scc.vertices.each{ |v| v.node.leader = @leader }
+
+        @mst.delete_scc(edge.scc) # remove SCC from the list
+      else
+        # change leaders of this SCC
+        @vertices.each{ |v| v.node.leader = edge.scc.leader }
+      end
     else
-      # change leaders of this SCC
-      @vertices.each{ |v| v.node.leader = other.leader }
-      other.vertices_count += @vertices_count
+      @vertices << edge.node1
+      @vertices << edge.node2
+      @edges << edge
     end
-  end
+
+    edge.scc = self # save link to the SCC in the edge itself
+  end  
 end
 
 class MST
   def initialize()
+    @sccs = [] # consists of many SCCs
     @mst = []
-    @sccs = []
   end
   
+  def delete_scc(scc)
+    @sccs.delete scc
+    puts 'removed scc from list'
+  end
+
   def push_edge(edge)
     insert_to_scc = @sccs.detect{|scc| scc.is_fit?(edge)}
     
     if insert_to_scc
       insert_to_scc.push_edge(edge)
     else # if edge is disconnected from any existing SCC then create a new one
-      scc = SCC.new
+      scc = SCC.new(self)
       scc.push_edge(edge)
       @sccs << scc
+      puts 'created new SCC; now SCC.count is - ' + @sccs.count.to_s
     end
-    
-    if @mst.empty? 
-      edge.node1.leader = edge.node2.leader
-    else
-      edge.node2
-    end
-    
+  
     @mst << edge
   end 
   
-  def check_loops(edge)
-    root1 = edge.node1.leader 
-    root2 = edge.node2.leader
-    
-    while root1 != root2 do
-      root1 = edge.node1.leader 
-      root2 = edge.node2.leader
-      break if root1 == root2
-      
+  def create_a_loop?(edge)
+    @sccs.each do |scc|
+      return true if edge.node1.leader == scc.leader || edge.node2.leader == scc.leader
     end
-    
+
     return false
   end
   
   def count
-    @mst.count
+    #@sccs.inject(0){|sum, scc| sum += scc.count}
+    @mst.count    
   end
 end
 
 mst = MST.new
 
 # sort edges by their cost in increasing order
-edges.sort{|m,n| m[1].cost <=> n[1].cost}.each do |edge|
-  mst.push_edge(edge) if ! mst.check_loops(edge)
+edges.sort{|m,n| m[1].cost <=> n[1].cost}.each_with_index do |edge, i|
+  puts "done with #{i} edges" if (i+1) % 10000 == 0
+  mst.push_edge(edge[1]) if ! mst.create_a_loop? edge[1]
 end
 
-puts mst.count
+puts 'Edges in MST - ' + mst.count.inspect
